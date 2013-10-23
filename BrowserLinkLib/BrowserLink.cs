e4b2f5fb7d943afa71dcc649a1381a48a916109e
Masahide Sakamaki <yasami@falys.com>
2013-10-23 19:17:11 +0900

    Track changing of Microsoft.Owin.Hosting (a.k.a Katana)

--- BrowserLinkLib/BrowserLink.cs
+++ BrowserLinkLib/BrowserLink.cs
@@ -4,51 +4,52 @@ using Microsoft.AspNet.SignalR;
 
 namespace Falys.BrowserLinkLib
 {
-	public class BrowserLink : IDisposable
-	{
-		private const string DefaultScheme = @"http";
+    public class BrowserLink : IDisposable
+    {
+        private const string DefaultScheme = @"http";
 
-		private const string DefaultHost = @"localhost";
+        private const string DefaultHost = @"localhost";
 
-		private const int DefaultPort = 8080;
+        private const int DefaultPort = 8080;
 
-		private readonly int _port;
-		private IDisposable _webApp;
+        private readonly int _port;
+        private IDisposable _webApp;
 
-		public Uri Url { get; private set; }
+        public Uri Url { get; private set; }
 
-		public BrowserLink(string scheme = DefaultScheme, string host = DefaultHost, int port = DefaultPort)
-		{
-			_port = port;
-			Url = new Uri(scheme + "://" + host + ":" + port);
-		}
+        public BrowserLink(string scheme = DefaultScheme, string host = DefaultHost, int port = DefaultPort)
+        {
+            _port = port;
+            Url = new Uri(scheme + "://" + host + ":" + port);
+        }
 
-		public void Start()
-		{
-			_webApp = WebApp.Start<Startup>(_port);
-		}
+        public void Start()
+        {
+            var startOptions = new StartOptions() { Port = _port };
+            _webApp = WebApp.Start<Startup>(startOptions);
+        }
 
-		public void Send(string msg)
-		{
-			var context = GlobalHost.ConnectionManager.GetHubContext<BrowserLinkHub>();
-			context.Clients.All.SendMessage(msg);
-		}
+        public void Send(string msg)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<BrowserLinkHub>();
+            context.Clients.All.SendMessage(msg);
+        }
 
-		/// <summary>
-		/// 使用中のリソースをすべてクリーンアップします。
-		/// </summary>
-		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
-		protected virtual void Dispose(bool disposing)
-		{
-			if (disposing && (_webApp != null))
-			{
-				_webApp.Dispose();
-			}
-		}
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing && (_webApp != null))
+            {
+                _webApp.Dispose();
+            }
+        }
 
-		public void Dispose()
-		{
-			Dispose(true);
-		}
-	}
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
 }
